4ad959eacc4937aed789b403303476afaadc19b4
nateGeorge <wordsforthewise@gmail.com>
2018-09-05 09:50:24 -0600

    remove commented code, add a few comments, functionize

--- nlp_processing.py
+++ nlp_processing.py
@@ -29,90 +29,65 @@ text = """This will be a test of writing terribly, and is going to be an example
             I will be using a future progressive verb, horribly.
             This is a passive sentence."""
 
-doc = nlp(text)
 
-sents = list(doc.sents)
-print("Number of Sentences = ", len(sents))
+def process_text(text):
+    """
+    Uses NLP to get passive voice sentences, sentences with adverbs, and progressive tense sentences.
+    Works ok so far, but not great -- lots of false positives.
+    """
+    doc = nlp(text)
 
-# gives match_id, start, end
-matches = matcher(doc)
-df = pd.DataFrame(matches, columns=['id', 'start', 'end'])
-df.drop_duplicates(inplace=True)
-print(df.shape[0], 'passive sentences detected')
+    sents = list(doc.sents)
+    print("Number of Sentences = ", len(sents))
 
-# print out passive phrases
-for i, r in df.iterrows():
-    print(doc[r['start']:r['end']])
+    # gives match_id, start, end
+    matches = matcher(doc)
+    df = pd.DataFrame(matches, columns=['id', 'start', 'end'])
+    df.drop_duplicates(inplace=True)
+    print(df.shape[0], 'passive sentences detected')
 
-# progressive tense detection
-# get verb phrase from sentences and check if progressive
+    # print out passive phrases
+    for i, r in df.iterrows():
+        print(doc[r['start']:r['end']])
 
-# adverb detection
-# "The road to hell is paved with adverbs." -- Stephen King
-
-verb_clause_pattern = r'<VERB>*<ADV>*<PART>*<VERB>+<PART>*'
-
-progressive_sentences, progressive_verb_clauses = [], []
-adverb_sentences, adverbs = [], []
-for s in sents:
-    verb_clauses = list(textacy.extract.pos_regex_matches(s, verb_clause_pattern))
-    for v in verb_clauses:
-        if len(v) > 1:  # need to have some helper verbs to have a problem
-            verb_tenses = pd.DataFrame(list(tenses(v.text)))
-            if 'progressive' in verb_tenses.iloc[:, -1].tolist():  # last column
-                progressive_sentences.append(s)
-                progressive_verb_clauses.append(v)
-
-    # was using this to detect verb clauses before, very inefficient
-    pos = np.array([w.pos_ for w in s])
-    # verbs = np.argwhere(pos == 'VERB').flatten()
-    # # get continuous verb clauses
-    # verb_diffs = np.concatenate((np.array([1]), np.diff(verbs)))
-    # verb_chunks = []
-    # temp_verb_chunk = []
-    # for i, v in enumerate(verb_diffs):
-    #     # print(s[verbs[i]].text)
-    #     # first one will always be 1
-    #     if v == 1:
-    #         temp_verb_chunk.append(s[verbs[i]].text)
-    #     else:
-    #         if len(temp_verb_chunk) > 1:
-    #             verb_ch = ' '.join(temp_verb_chunk)
-    #         else:
-    #             verb_ch = temp_verb_chunk[0]
-    #
-    #         print(verb_ch)
-    #
-    #         verb_chunks.append(verb_ch)
-    #         temp_verb_chunk = [s[verbs[i]].text]
-    #
-    # if len(temp_verb_chunk) > 1:
-    #     verb_ch = ' '.join(temp_verb_chunk)
-    # else:
-    #     verb_ch = temp_verb_chunk[0]
-    #
-    # verb_chunks.append(verb_ch)
+    # progressive tense detection
+    # get verb phrase from sentences and check if progressive
 
 
-    adverb_idxs = np.argwhere(pos == 'ADV').flatten()
+    verb_clause_pattern = r'<VERB>*<ADV>*<PART>*<VERB>+<PART>*'
 
-    if len(adverb_idxs) != 0:
-        adverb_sentences.append(s)
-        adverbs.append([s[a] for a in adverb_idxs])
+    progressive_sentences, progressive_verb_clauses = [], []
+    adverb_sentences, adverbs = [], []
+    for s in sents:
+        verb_clauses = list(textacy.extract.pos_regex_matches(s, verb_clause_pattern))
+        for v in verb_clauses:
+            if len(v) > 1:  # need to have some helper verbs to have a problem
+                verb_tenses = pd.DataFrame(list(tenses(v.text)))
+                if 'progressive' in verb_tenses.iloc[:, -1].tolist():  # last column
+                    progressive_sentences.append(s)
+                    progressive_verb_clauses.append(v)
 
-# print out sentences with adverbs and the adverb
+        # adverb detection
+        # "...the road to hell is paved with adverbs..." -- Stephen King
+        pos = np.array([w.pos_ for w in s])
+        adverb_idxs = np.argwhere(pos == 'ADV').flatten()
 
-print('\n\n\nADVERBS')
-print(len(adverb_sentences), 'sentences detected with adverbs\n')
-if len(adverb_sentences) > 0:
-    for i, adv in enumerate(adverb_sentences):
-        print(adv)
-        print('has adverbs:', adverbs[i], '\n')
+        if len(adverb_idxs) != 0:
+            adverb_sentences.append(s)
+            adverbs.append([s[a] for a in adverb_idxs])
 
-print('\n\n\nPRORGESSIVE TENSES')
+    # print out sentences with adverbs and the list of adverbs
+    print('\n\n\nADVERBS')
+    print(len(adverb_sentences), 'sentences detected with adverbs\n')
+    if len(adverb_sentences) > 0:
+        for i, adv in enumerate(adverb_sentences):
+            print(adv)
+            print('has adverbs:', adverbs[i], '\n')
 
-print(len(progressive_sentences), 'sentences detected with progressive verb clauses\n')
-if len(progressive_sentences) > 0:
-    for i, adv in enumerate(progressive_sentences):
-        print(adv)
-        print('has progressive verb clause(s):', progressive_verb_clauses[i], '\n')
+    # print out sentences with possible progressive verb clauses
+    print('\n\n\nPRORGESSIVE TENSES')
+    print(len(progressive_sentences), 'sentences detected with progressive verb clauses\n')
+    if len(progressive_sentences) > 0:
+        for i, adv in enumerate(progressive_sentences):
+            print(adv)
+            print('has progressive verb clause(s):', progressive_verb_clauses[i], '\n')
